517495af68f926b69c5233cbbd28bd747c572eb4
Rathan Kumar <rathan.kumar492@gmail.com>
2019-12-26 20:46:24 +0530

    Implemented bootstrap for homepage and showed the jobs dynamically

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+# Get access to the model to pass the variable to views or home
+from .models import Job
 # Create your views here.
+
+
+def home(request):
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
