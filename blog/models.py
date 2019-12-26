9ca4edaa77b13ccb399ad0e763b972bb61ea91e8
Rathan Kumar <rathan.kumar492@gmail.com>
2019-12-27 01:06:33 +0530

    blogs, static files done

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,10 @@ from django.db import models
 # import datetime
 
 # Create your models here.
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField()
-    body = models.TextField(max_length=200)
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
