8e387fbe42d89a0ad21424e91e9923cc86732744
Rathan Kumar <rathan.kumar492@gmail.com>
2019-12-27 20:50:37 +0530

    one step behind blog to complete

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+
 # import datetime
 
 # Create your models here.
@@ -9,3 +11,6 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
